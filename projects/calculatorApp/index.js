8f0c495f1fad65a83c048bcebd8c2d5cc5371382
mothibimalebana <mothibi.mm12@gmail.com>
2024-06-25 20:01:53 +0200

    defined variables

--- projects/calculatorApp/index.js
+++ projects/calculatorApp/index.js
@@ -2,6 +2,10 @@
 let previousScreenV, currentScreenValue;
 let numbers = document.querySelectorAll(".numbers");
 let operators = document.querySelectorAll(".operators");
+let inputScreen = document.querySelector(".inputScreen");
+let C = document.querySelector("#C");
+let eqauls = document.querySelector("#numEqauls");
+let comma = document.querySelector("#decimal");
 
 //Operator Functions
 function add(num1, num2){
@@ -20,7 +24,28 @@ function divide(num1, num2){
 function operate(op, num1, num2){
 	switch(op){
 		case '+':
-			add(num1, num2)
+			add(num1, num2);
 			console.log(num1, num2, add(num1, num2));
+			break;
+		case '-':
+			subtract(num1, num2);
+			console.log(num1, num2, subtract(num1, num2));
+			break;
+		case '*':
+			multipy(num1, num2);
+			console.log(num1, num2, multipy(num1, num2));
+			break;
+		case '/':
+			divide(num1, num2);
+			console.log(num1, num2, divide(num1, num2));
+			break;
 	}
+}
+
+//Display Screen
+numbers.forEach((numbers) => {
+	numbers.addEventListener("click", () => displayNumbers(numbers.textContent))
+})
+function displayNumbers(num){
+	inputScreen.textContent += num
 }
