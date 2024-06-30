0130fc38f9e9786b2d5ce42e9351a7792b43929e
mothibimalebana <mothibi.mm12@gmail.com>
2024-06-30 19:30:09 +0200

    operators and numbers are now functional
    added event onClick event listeners for numbers and operators variables

--- projects/calculatorApp/index.js
+++ projects/calculatorApp/index.js
@@ -7,6 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
 	let numEqauls = document.querySelector("#numEqauls");
 	let del = document.querySelector("#del");
 	let decimal = document.querySelector("#decimal");
+	let inputScreen = document.querySelector(".inputScreen");
+	C.addEventListener("click", () => {
+	prevValue = '';
+	currValue = '';
+	op = '';
+	inputScreen.textContent = '';
+	})
+	
 
 	let numbers = document.querySelectorAll(".numbers");
 	let operators = document.querySelectorAll(".operators");
@@ -14,11 +22,29 @@ document.addEventListener("DOMContentLoaded", () => {
 	numbers.forEach((numbers) => {
 		numbers.addEventListener("click", () => {
 			handleNumbers(numbers.textContent);
+			inputScreen.textContent = currValue;
 		});
+	})
+	operators.forEach((operators) => {
+		operators.addEventListener("click", () => {
+			handleOperators(operators.textContent)
+			inputScreen.textContent = currValue;
+		})
 	})
 })
 
 //functions
 function handleNumbers(num){
-	console.log(num);
+	if(currValue.length <= 7){
+	currValue += num;
+	}
+}
+function handleOperators(operator){
+	op = operator;
+	prevValue = currValue;
+	currValue = '';
+}
+function operate(num1, num2){
+	currValue = Number(currValue);
+	prevValue = Number(prevValue);
 }
