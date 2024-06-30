e92d321e2f3bfa5b63f4de012141d1c0fd743258
mothibimalebana <mothibi.mm12@gmail.com>
2024-06-30 19:08:43 +0200

    added: event listener to all numbers
    when a number is clicked the textContent is displayed to the console log

--- projects/calculatorApp/index.js
+++ projects/calculatorApp/index.js
@@ -1,1 +1,24 @@
-console.log("Hello world")
+let currValue = '';
+let prevValue = '';
+let op = '';
+
+document.addEventListener("DOMContentLoaded", () => {
+	let C = document.querySelector("#C");
+	let numEqauls = document.querySelector("#numEqauls");
+	let del = document.querySelector("#del");
+	let decimal = document.querySelector("#decimal");
+
+	let numbers = document.querySelectorAll(".numbers");
+	let operators = document.querySelectorAll(".operators");
+
+	numbers.forEach((numbers) => {
+		numbers.addEventListener("click", () => {
+			handleNumbers(numbers.textContent);
+		});
+	})
+})
+
+//functions
+function handleNumbers(num){
+	console.log(num);
+}
