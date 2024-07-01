08bde360ff9d7bc6c1d287785a24ee60c31eb802
mothibimalebana <mothibi.mm12@gmail.com>
2024-07-01 15:22:38 +0200

    created: add(num1, num2), subtract(num1, num2), multiply(num1, num2), divide(num1, num2)
    added functions for mathematical operands

--- projects/calculatorApp/index.js
+++ projects/calculatorApp/index.js
@@ -1,4 +1,45 @@
+/**Variables */
 let currValue = '';
 let prevValue = '';
 let op = '';
 let result;
+
+/**Functions */
+//mathematical operands
+function add(num1, num2){
+	return num1 + num2
+}
+function subtract(num1, num2){
+	return num1 - num2
+}
+function multiply(num1, num2){
+	return num1 * num2
+}
+function divide(num1, num2){
+	if (num2 === 0){
+		alert("division by zero is not possible");
+	}
+	return num1 / num2
+}
+
+//operate function
+function operate(op, num1, num2){
+	switch(op){
+		case '+':
+			result = add(num1, num2);
+			console.log(result);
+			break;
+		case '-':
+			result = subtract(num1, num2);
+			console.log(result);
+			break;
+		case '*':
+			result = multiply(num1, num2);
+			console.log(result);
+			break;
+		case '/':
+			result = divide(num1, num2);
+			console.log(result);
+			break;
+	}
+}
