d4ba2699a594d7121ea78638f3d6bca54d6566da
mothibimalebana <mothibi.mm12@gmail.com>
2024-09-19 12:01:09 +0200

    created book object constructor

--- practice/objects/js/index.js
+++ practice/objects/js/index.js
@@ -1,2 +1,10 @@
-let user = {};
-user.name = 'John';
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info() = function() {
+        console.log(this.title, this.author, this.pages, this.read);
+    };
+}
