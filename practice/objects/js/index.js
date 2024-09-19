71f28209113e9f3b336ee763157f05f90d978dd9
mothibimalebana <mothibi.mm12@gmail.com>
2024-09-19 14:48:01 +0200

    Book constructor

--- practice/objects/js/index.js
+++ practice/objects/js/index.js
@@ -4,7 +4,9 @@ function Book(title, author, pages, read){
     this.pages = pages;
     this.read = read;
 
-    this.info() = function() {
+    this.info = function() {
         console.log(this.title, this.author, this.pages, this.read);
     };
 }
+
+let theHobit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
