64ff7c37ab8f4c6c65bdcc3f70d8d4ee618c280f
mothibimalebana <mothibi.mm12@gmail.com>
2024-09-21 20:25:41 +0200

    created display books function

--- projects/libraryProject/js/index.js
+++ projects/libraryProject/js/index.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+const myLibrary = ['The Subtle Art Of Not Giving A Fuck', 'The Alchemist', 'Atomic Habits'];
+
+function Book(title, author, pages, read) {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayBooks(libraryArry) {
+    libraryArry.forEach((element) => {
+
+    });
+}
