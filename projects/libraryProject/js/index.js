4b746be72d94332ee0900770ed2cae98c4812bc8
mothibimalebana <mothibi.mm12@gmail.com>
2024-09-28 16:53:49 +0200

    changed layout of the collection section

--- projects/libraryProject/js/index.js
+++ projects/libraryProject/js/index.js
@@ -1,8 +1,16 @@
-const myLibrary = ['The Subtle Art Of Not Giving A Fuck', 'The Alchemist', 'Atomic Habits'];
+const myLibrary = ['Forget A mentor, Find a Sponsor', 'The Time Has Come', 'Pride And Protest'];
 
-function Book(title, author, pages, read) {
-
+function Book(title, author, pages, read,) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
+
+const bookOne = new Book('The Time Has Come', 'Nikki Payne', 457, false);
+const bookTwo = new Book('Forget A Mentor Find A Sponsor', 'Spensor Ann', 256, false);
+const bookThree = new Book('Pride And Protest', 'Tim Bell', 870, false);
+
 
 function addBookToLibrary() {
 
