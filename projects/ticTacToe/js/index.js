8af41af22c2ec648345cf13d6aa9e69586e3f046
mothibimalebana <mothibi.mm12@gmail.com>
2024-10-09 16:02:04 +0200

    created 2d 3*3 grid using arrays

---
+++ projects/ticTacToe/js/index.js
@@ -0,0 +1,16 @@
+const gameBoard = (() =>{
+    const rows = 3;
+    const columns = 3;
+    board = [];
+
+    for(let i = 0; i < rows ; i++){
+        board[i] = [];
+        for(let j = 0; j < columns; j++){
+            board[i].push(cell());
+        }
+    }
+})();
+
+const cell = (() => {
+    let value = 0;
+})();
