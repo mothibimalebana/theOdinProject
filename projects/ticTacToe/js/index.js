feaba7feaaab7e4ec208a9ab76c23b47f89c6a49
mothibimalebana <mothibi.mm12@gmail.com>
2024-10-09 21:34:52 +0200

    working on the availability of each cell, to avoid collision

--- projects/ticTacToe/js/index.js
+++ projects/ticTacToe/js/index.js
@@ -1,16 +1,61 @@
-const gameBoard = (() =>{
+const gameBoard = (function () {
     const rows = 3;
     const columns = 3;
-    board = [];
+    const board = [];
 
-    for(let i = 0; i < rows ; i++){
+    for(let i = 0; i < rows; i++){
         board[i] = [];
         for(let j = 0; j < columns; j++){
-            board[i].push(cell());
+            board[i].push(cell())
         }
     }
+
+    const getBoard = () => board;
+
+    const printGameBoard = () => {
+        const boardCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardCellValues);
+    }
+
+    return{getBoard, printGameBoard}
 })();
 
-const cell = (() => {
+function cell(){
     let value = 0;
-})();
+
+    const getValue = () => value;
+
+    const addToken = (player) => {
+        value = player;
+    }
+
+    return {getValue, addToken}
+}
+
+function gameController (
+    playerOne = "Player One",
+    playerTwo = "Player Two"
+) {
+    const players = [
+        {
+            name: playerOne,
+            token: 1,
+
+        },
+        {
+            name: playerTwo,
+            token: 2,
+
+        }
+    ]
+
+    const placetoken = (row, column, player) => {
+        gameBoard.getBoard()[row][column].addToken(player)
+    }
+
+    const availableCell = gameBoard.getBoard().map((row) => row.filter((cell) => cell.getValue() === 0));
+
+    if(!availableCell.length) return 
+
+    return {placetoken, availableCell}
+}
