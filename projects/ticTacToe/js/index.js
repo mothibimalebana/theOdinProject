daaf859b6730b27876ef35d9b73b1c97502a3b4f
mothibimalebana <mothibi.mm12@gmail.com>
2024-10-10 10:53:15 +0200

    created switchPlayer method to gameController IIFE
    this method switches between players after a round has been played

--- projects/ticTacToe/js/index.js
+++ projects/ticTacToe/js/index.js
@@ -6,10 +6,17 @@ const gameBoard = (function () {
     for(let i = 0; i < rows; i++){
         board[i] = [];
         for(let j = 0; j < columns; j++){
-            board[i].push(cell)
+            board[i].push(cell())
         }
     }
 
+    const getBoard = () => board;
+
+    const printBoard = () => {
+        
+    }
+
+    return {getBoard, }
 
 })();
 
@@ -26,7 +33,7 @@ const gameController = (function (
     playerOne = 'PlayerOne',
     playerTwo = 'PlayerTwo'
 ){
-    players[
+    const players = [
         {
             name: playerOne,
             token: 1
@@ -36,4 +43,18 @@ const gameController = (function (
             token: 2
         }
     ]
+    let currentPlayer = players[0];
+
+    const switchPlayer = () => {
+        if(currentPlayer === players[0]) {
+            currentPlayer = players[1];
+        }
+        else if(currentPlayer === players[1]) {
+            currentPlayer === players[0];
+        }
+    }
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    return {players, switchPlayer, getCurrentPlayer}
 })();
