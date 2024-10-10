2deaef8eaba1dd6b0d8b57a42631c2fa922b2a1f
mothibimalebana <mothibi.mm12@gmail.com>
2024-10-10 10:00:52 +0200

    created players array
    player array contains objects of playerOne and playerTwo

--- projects/ticTacToe/js/index.js
+++ projects/ticTacToe/js/index.js
@@ -9,5 +9,31 @@ const gameBoard = (function () {
             board[i].push(cell)
         }
     }
+
+
 })();
 
+function cell() {
+    let value = 0;
+
+    getValue = () => value;
+    addToken = (token) => { value = token};
+
+    return {getValue, addToken}
+}
+
+const gameController = (function (
+    playerOne = 'PlayerOne',
+    playerTwo = 'PlayerTwo'
+){
+    players[
+        {
+            name: playerOne,
+            token: 1
+        },
+        {
+            name: playerTwo,
+            token: 2
+        }
+    ]
+})();
