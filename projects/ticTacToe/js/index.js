0cbe3cf022b3a37058b65843f3be1c971531acc8
mothibimalebana <mothibi.mm12@gmail.com>
2024-10-10 09:49:05 +0200

    created 3*3 board using arrays
    each element in the 3*3 contains a cell object which allows us to change and getValue of the token

--- projects/ticTacToe/js/index.js
+++ projects/ticTacToe/js/index.js
@@ -1,6 +1,13 @@
 const gameBoard = (function () {
     const rows = 3;
     const columns = 3;
+    board = [];
 
-    
+    for(let i = 0; i < rows; i++){
+        board[i] = [];
+        for(let j = 0; j < columns; j++){
+            board[i].push(cell)
+        }
+    }
 })();
+
