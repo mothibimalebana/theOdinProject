3657390d6b7fc7ca570f5cc5e19e60779b146a1e
mothibimalebana <mothibi.mm12@gmail.com>
2024-10-15 08:37:48 +0200

    created cell factory function which contains state of each cell
    each grid in the 3x3 array contains a cell()

--- projects/ticTacToe/js/index.js
+++ projects/ticTacToe/js/index.js
@@ -1,12 +1,25 @@
-(function gameBoard() {
+const gameBoard = (function () {
     const row = 3;
     const column = 3;
     let board = [];
 
     for(let i = 0; i < row; i++){
-        board = [];
+        board[i] = [];
         for(let j = 0; j < column; j++){
-            board.push(console.log(`row: ${i} \ncolumn: ${j}`))
+            board[i].push(cell())
         }
     }
+
+    const getBoard = () => board;
+
+    return {getBoard}
 })();
+
+function cell(){
+    let value = 0;
+
+    const getValue = () => value;
+    const changeValue = (playerValue) => {value = playerValue};
+
+    return{getValue, changeValue}
+}
