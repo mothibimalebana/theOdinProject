235765643bd1ef84225f72a66b3f2bc8afb174bf
mothibimalebana <mothibi.mm12@gmail.com>
2024-10-26 23:11:52 +0200

    installed and added style-loader, css-loader to webpack.config.js
    this will help bundle css files

---
+++ projects/Resto/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules:[
+            {
+                test: /\.css$/i,
+                rule: ['style-loader', 'css-loader'],
+                
+            },
+        ],
+    },
+}
