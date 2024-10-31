48f06c43ddcaafbc0c4d719fb4cb1bb0b59024ad
mothibimalebana <mothibi.mm12@gmail.com>
2024-10-31 23:08:55 +0200

    managed to fix title and title span
    the text was placed inside of the title div, instead of the h4

--- projects/Resto/src/homePage.js
+++ projects/Resto/src/homePage.js
@@ -16,15 +16,30 @@ const homePage = function (){
     const title = document.createElement("div");
     title.classList.add("title");
     const titleH4 = document.createElement("h4");
+    titleH4.textContent = "Home made healthy, "
     const titleSpan = document.createElement("span");
-    titleH4.textContent = 'vegan food';
+    titleSpan.textContent = 'vegan food';
     titleSpan.style.color = '#2A9D8F';
     titleH4.appendChild(titleSpan);
     title.appendChild(titleH4);
     contentLeft.appendChild(title)
+    
     //contentLeft:button
+    const buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("button");
+    const buttonLeft = document.createElement("button");
+    buttonLeft.textContent = "View Menu";
+    buttonLeft.id = "buttonLeft";
+    buttonDiv.appendChild(buttonLeft);
+    const buttonRight = document.createElement("button");
+    buttonRight.textContent = "Local Nearby";
+    buttonDiv.appendChild(buttonRight);
+    contentLeft.appendChild(buttonDiv);
 
-
+    //contentRight
+    const imgRight = document.createElement("img");
+    imgRight.src = "./img/korean-dish.png";
+    contentRight.appendChild(imgRight);
 }
 
 export {homePage}
