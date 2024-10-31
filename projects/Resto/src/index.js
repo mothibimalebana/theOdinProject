3ac2e817406bd0f528d74298f7714bb93980df36
mothibimalebana <mothibi.mm12@gmail.com>
2024-10-31 19:40:25 +0200

    for some reason mainPage IFFFEE  is giving me problems
    I can't find teh Iffe in the console log

--- projects/Resto/src/index.js
+++ projects/Resto/src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello, Webpack");
+import './css/styles.css';
+
+const mainPage = (function () {
+    const content = document.getElementById("content");
+    const getContent = () => content;
+
+    return{getContent}
+})();
